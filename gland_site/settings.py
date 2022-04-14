7cc26361d11e9706a6cf1bf09c4fa2591f45e3aa
Sharp_Mind <redlance988@gmail.com>
2022-04-14 23:10:05 +0300

    DEBUG paramerter changed in settings.py

--- gland_site/settings.py
+++ gland_site/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9u18$ki(zkh6s_^=08=zudslsf2hdlz(ns96bvwgo1wz03a8uw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = TrueDEBUG = False if os.getenv("DJANGO_PRODUCTION", default=None) else True
 
 ALLOWED_HOSTS = ["*"]
 
