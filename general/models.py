dd23482dad7c6da8be2f3d56f3661ce05406e359
root <root@188-93-211-60.cloudvps.regruhosting.ru>
2022-04-25 18:53:13 +0300

    database installed

--- general/models.py
+++ general/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    name = models.CharField(verbose_name="имя", max_length=64, unique=True)
+    date_in = models.DateField(verbose_name="дата добавления", auto_now_add=True)
+    phone = models.BigIntegerField(verbose_name="номер телефона", unique=True, default=' - ')
+    elmail = models.EmailField(verbose_name="элетронная почта", unique=True)
+    note = models.TextField(verbose_name="заметка", blank=True)
+    is_sended = models.BooleanField(verbose_name="отправлено")
+    expire = models.DateField(verbose_name="дата истечения")
+    vvcard = models.IntegerField(verbose_name="номер карты ВВ", default="НЕТ КАРТЫ")
+
+    def __str__(self):
+        return self.name
