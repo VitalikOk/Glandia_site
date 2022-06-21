b617b22e969b7ff78eae2014f693fe77e71e21b7
Sharp_Mind <redlance988@gmail.com>
2022-06-21 23:43:37 +0300

    database param in settings.py fixed

--- gland_site/settings.py
+++ gland_site/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = 'gland_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#         "default": {
-#             "NAME": "glandia",
-#             "ENGINE": "django.db.backends.postgresql",
-#             "USER": "django",
-#             "PASSWORD": "glandia_site",
-#             "HOST": "localhost",
-#         }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+        "default": {
+            "NAME": "glandia",
+            "ENGINE": "django.db.backends.postgresql",
+            "USER": "django",
+            "PASSWORD": "glandia_site",
+            "HOST": "localhost",
+        }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
